3d153c0b5b156fc3854b4a4ddf9a9bb318733865
Dipjyoti Sikder <dipjyoti.sikder@bjitgroup.com>
2023-04-25 10:47:21 +0600

    Added API for Item creation

--- ToDoQL/Controllers/ItemsController.cs
+++ ToDoQL/Controllers/ItemsController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotChocolate.Subscriptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using ToDoQL.Data;
+using ToDoQL.GraphQL;
+using ToDoQL.Models;
 
 namespace ToDoQL.Controllers
 {
@@ -6,11 +12,46 @@ namespace ToDoQL.Controllers
     [ApiController]
     public class ItemsController : ControllerBase
     {
+        private readonly AppDbContext _context;
+        private readonly ITopicEventSender _topicEventSender;
+
+        public ItemsController(
+            AppDbContext context,
+            ITopicEventSender topicEventSender)
+        {
+            _context = context;
+            _topicEventSender = topicEventSender;
+        }
 
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
-            return Ok("Running");
+            return Ok(await _context.Items.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddItemInput input)
+        {
+            var item = new Item
+            {
+                Title = input.title,
+                Description = input.description,
+                IsDone = input.isDone,
+                ListId = input.listId,
+            };
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+
+            var response = new AddItemResponse(
+                item.Id,
+                item.Title,
+                item.Description,
+                item.IsDone);
+
+            var itemCreationTopic = $"{"ItemCreated"}";
+            await _topicEventSender.SendAsync(itemCreationTopic, response);
+
+            return Ok(response);
         }
     }
 }
